edeef70e68a2ca17fc991731f2ddb28674ddf81d
Vinicius Santana <santanaviny@gmail.com>
2017-02-23 04:29:24 -0800

    Toubleshooting list fetch with refetching queries

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from "react-apollo";
 import gql from 'graphql-tag';
 import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component{
 
@@ -17,7 +18,8 @@ class SongCreate extends Component{
     this.props.mutate({
       variables: {
         title: this.state.title
-      }
+      },
+      refetchQueries: [{ query }]
     }).then(() => hashHistory.push('/'));
   }
 
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
   renderSongs() {
@@ -34,14 +35,5 @@ class SongList extends Component {
     );
   }
 }
-
-const query = gql`
-  {
-    songs {
-      id,
-      title
-    }
-  }
-`;
 
 export default graphql(query)(SongList);
