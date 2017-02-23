f12919e8e5c51a6cf306be2d57baa62bc156d3e0
Vinicius Santana <santanaviny@gmail.com>
2017-02-23 01:14:50 -0800

    MongoLab Setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://vini:vini@ds161059.mlab.com:61059/lyricalgqldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
