78351c475ea5a2671eadeaa7a38f9a52699c2fd0
Vinicius Santana <santanaviny@gmail.com>
2017-02-23 11:37:36 -0800

    Quering a particular song

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render(){
+    console.log(this.props);
+
     return (
       <div>
         <h3>Song Detail</h3>
@@ -10,4 +14,6 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
