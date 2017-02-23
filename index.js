f98b7d1b7c78d503576b9c2ce11b8009333110f7
Vinicius Santana <santanaviny@gmail.com>
2017-02-23 05:03:01 -0800

    Settup for heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./server/server');
 
-app.listen(4000, () => {
-  console.log('Listening');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log('Listening to port ' + PORT);
 });
